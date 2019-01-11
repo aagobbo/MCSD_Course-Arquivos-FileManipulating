ecd0b282076090645c90af6d53b5a4c2f487c697
Adriano Antonio Gobbo <gobbo.email@gmail.com>
2019-01-11 16:07:26 -0200

    File Manipulation course MCSD

---
+++ Mind.Arquivos/Mind.Arquivos.Aplicativo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Mind.Arquivos.Dominio;
+using System;
+using System.Linq;
+using System.IO;
+
+namespace Mind.Arquivos.Aplicativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VarredorEstrutura.VarreDiretorio(@"C:\Allfiles").Where(r => r is FileInfo).GroupBy(g => g.Extension).Select(r => new { r.Key, ExtensionCount = r.Count() }).ToList().ForEach(i => Console.WriteLine("Tipo: {0}, Total: {1}", i.Key, i.ExtensionCount));
+            //foreach (var res in resultado)
+            //    Console.WriteLine(res.FullName);
+            //Console.ReadLine();
+
+            //foreach (var res in resultado)
+            //{
+            //    Console.WriteLine("Tipo: {0}, Total: {1}", res.Key, res.ExtensionCount);
+            //}
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ Mind.Arquivos/Mind.Arquivos.Dominio/VarredorEstrutura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Mind.Arquivos.Dominio
+{
+    public abstract class VarredorEstrutura
+    {
+        public static FileSystemInfo[] VarreDiretorio(string caminho)
+        {
+            List<string> resultado = new List<string>();
+            if (!Directory.Exists(caminho))
+                throw new Exception("não existe essa pasta");
+            DirectoryInfo diretorio = new DirectoryInfo(caminho);
+            return diretorio.GetFileSystemInfos("*", SearchOption.AllDirectories);
+
+        }
+
+
+    }
+}
